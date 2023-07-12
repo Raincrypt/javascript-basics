c8ddddc2190c1185c34aeb5be5725ca244fa2381
Mousom <sarkarmousom8@gmail.com>
2023-07-13 00:45:02 +0530

    Next commit

---
+++ 5-JS/script4.js
@@ -0,0 +1,9 @@
+//Importing and exporting
+
+import { order, laptopPrise, q } from './products.js'
+
+console.log("Importing Module")
+
+order("Mousom", "Macbook Pro")
+console.log(`Price of the Laptop is ${laptopPrise} and I bought ${q} of them.`)
+
