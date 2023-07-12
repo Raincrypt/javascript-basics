61204b6b3977f28ab77b5068f09b36f53acf638a
Mousom <sarkarmousom8@gmail.com>
2023-07-13 00:55:30 +0530

    Next commit

--- 5-JS/script4.js
+++ 5-JS/script4.js
@@ -2,11 +2,11 @@
 
 //import { order, laptopPrise as lp, q } from './products.js' //to import specific things
 import * as singleVariable from "./products.js"
-import order from "./products2.js"// as it was exported as default so not need to de-structure
+import anyName from "./products2.js"// as it was exported as default so not need to de-structure
 
 console.log("Importing Module")
 
 singleVariable.order("Mousom", "Macbook Pro")
 console.log(`Price of the Laptop is ${singleVariable.laptopPrise} and I bought ${singleVariable.q} of them.`)
 
-order("Aarushi", "ROG Zephyrus 14")
+anyName("Aarushi", "ROG Zephyrus 14")
